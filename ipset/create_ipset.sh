2012ffbf890408e2b4090a9383f93a5e48806b88
LukyanovM <43216037+LukyanovM@users.noreply.github.com>
2019-04-05 02:54:15 +0300

    Update create_ipset.sh
    Добавлена поддержка IPv6

--- ipset/create_ipset.sh
+++ ipset/create_ipset.sh
@@ -3,12 +3,13 @@
 
 SCRIPT=$(readlink -f $0)
 EXEDIR=$(dirname $SCRIPT)
-
+IPSET_OPT="hashsize 131072 maxelem 524288"
+IPSET_OPT6="family inet6 hashsize 131072 maxelem 524288"
 . "$EXEDIR/def.sh"
 
 create_ipset()
 {
-ipset flush $2 2>/dev/null || ipset create $2 $1 maxelem 262144
+ipset flush $2 2>/dev/null || ipset create $2 $1 $IPSET_OPT
 for f in "$3" "$4"
 do
  [ -f "$f" ] && {
@@ -22,6 +23,20 @@ do
 done
 return 0
 }
-
-create_ipset hash:net $ZIPSET $ZIPLIST $ZIPLIST_USER
-create_ipset hash:net $ZIPSET_IPBAN $ZIPLIST_IPBAN $ZIPLIST_USER_IPBAN
+create_ipset6()
+{
+ipset flush $2 2>/dev/null || ipset create $2 $1 $IPSET_OPT6
+for f in "$3" "$4"
+do
+ [ -f "$f" ] && {
+  echo Adding to ipset $2 \($1\) : $f
+  sort -u "$f" | sed -nre "s/^.+$/add $2 &/p" | ipset -! restore
+ }
+done
+return 0
+}
+create_ipset6 hash:ip $ZIPSET6 $ZIPLIST6 $ZIPLIST_USER6
+create_ipset hash:ip $ZIPSET $ZIPLIST $ZIPLIST_USER
+create_ipset hash:ip $ZIPSET_IPBAN $ZIPLIST_IPBAN $ZIPLIST_USER_IPBAN
+ipset save zapret -f /opt/zapret/ipset/zapret.ipset
+ipset save zapret6 -f /opt/zapret/ipset/zapret6.ipset
